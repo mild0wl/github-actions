5f48a6d3f0f06ff8bc1c3aa904e685d0950545a0
mild0wl <sthokala@umd.edu>
2024-11-25 18:09:26 -0500

    CI pytest#2

--- tests/test_substring.py
+++ tests/test_substring.py
@@ -17,5 +17,21 @@ class TestSubstring(unittest.TestCase):
         score:float = string_utils.calculate_match_degree("software engineering","building engineering")
         self.assertEqual(score, 0.6)
 
+    def test_no_common_substrings(self):
+        """No Common Substring and the degree should be Zero"""
+        score:float = string_utils.calculate_match_degree("abcdef","uvwxyz")
+        self.assertEqual(score, 0.0)
+
+    # def test_partial_overlap_substrings(self):
+    #     """Tests for partial overlap of substrings"""
+    #     score:float = string_utils.calculate_match_degree("saikrishna","saithokalakrishna")
+    #     expected_score = len("sai")/len("saithokalakrishna")
+    #     self.assertEqual(score, expected_score)
+
+    def test_empty_input_string(self):
+        """Test for empty input string"""
+        score:float = string_utils.calculate_match_degree("","testinput")
+        self.assertEqual(score, 0.0)
+
 if __name__ == "__main__":
     unittest.main()
